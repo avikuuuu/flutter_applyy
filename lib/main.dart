7ba636c557a403dc7d3b21c8687aa65d04923179
aviku11 <70461207+aviku11@users.noreply.github.com>
2021-11-27 17:02:28 +0530

    day3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_applyy/home_page.dart';
+import 'package:flutter_applyy/pages/home_page.dart';
+import 'package:flutter_applyy/pages/login_page.dart';
 
 void main() {
   runApp(MYAPP());
@@ -10,23 +11,35 @@ class MYAPP extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    int days = 30;
-    String name = "avdesh";
-    double pie = 3.14;
-    bool ismale = true;
-    num tem = 30.0; //num can take both int int and float variable
+    // int days = 30;
+    // String name = "avdesh";
+    // double pie = 3.14;
+    // bool ismale = true;
+    // num tem = 30.0; //num can take both int int and float variable
 
-    var app =
-        "avkuuuu"; // it cna take all things which is above int string doble then intrepreter automatically detect it
+    // var app =
+    //     "avkuuuu"; // it cna take all things which is above int string doble then intrepreter automatically detect it
 
-    const pieee =
-        22.4; //const use to put constant things in which can't be chnange in future
+    // const pieee =
+    //     22.4; //const use to put constant things in which can't be chnange in future
 
-    final ap = 34; //in FINAL content can modifye
+    // final ap = 34; //in FINAL content can modifye
+
+    // bringVegitable(thaila: true);
 
     return MaterialApp(
-      home:HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        // ignore: prefer_const_constructors
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
     );
   }
 }
